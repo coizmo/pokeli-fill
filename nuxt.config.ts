b66d188c7ff8c52e4d180ada6dc474823b63772a
coizmo <coizmo@gmail.com>
2024-04-02 00:36:56 +0900

    :wrench: Nuxt+Primevue+Tailwindcss のフロント環境構築

---
+++ nuxt.config.ts
@@ -0,0 +1,23 @@
+import path from "path";
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  ssr: false,
+  devtools: { enabled: true },
+  css: ["~/assets/css/main.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  build: {
+    transpile: ["vuetify"],
+  },
+  modules: ["nuxt-primevue"],
+  primevue: {
+    options: {
+      unstyled: true,
+    },
+    importPT: { from: path.resolve(__dirname, "./presets/lara/") },
+  },
+});
