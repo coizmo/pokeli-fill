771d2a5b41e0259955390856dcfb9af3bc1a7e2a
coizmo <coizmo@gmail.com>
2024-10-21 02:39:30 +0900

    :wrench: update nuxt to 3.13

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,25 +2,33 @@ import path from "path";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
+
   app: {
     baseURL: '/pokeli-fill/',
   },
+
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+
   build: {
     transpile: ["vuetify"],
   },
+
   modules: ["nuxt-primevue"],
+
   primevue: {
     options: {
       unstyled: true,
     },
     importPT: { from: path.resolve(__dirname, "./presets/lara/") },
   },
+
+  compatibilityDate: "2024-10-21",
 });
