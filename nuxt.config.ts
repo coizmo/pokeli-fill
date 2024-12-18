46e109d29d101df76be0dd9f2948174ef9508199
coizmo <coizmo@gmail.com>
2024-12-19 01:35:53 +0900

    :wrench: VueUse を導入

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,7 +21,7 @@ export default defineNuxtConfig({
     transpile: ["vuetify"],
   },
 
-  modules: ["nuxt-primevue"],
+  modules: ["nuxt-primevue", "@vueuse/nuxt"],
 
   primevue: {
     options: {
